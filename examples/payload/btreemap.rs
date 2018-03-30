11dfcf58025e3434e769528573632ca80b9cf83a
losfair <zhy20000919@hotmail.com>
2018-03-30 23:56:29 +0800

    Update example

--- examples/payload/btreemap.rs
+++ examples/payload/btreemap.rs
@@ -1,7 +1,7 @@
 use std::collections::BTreeMap;
 
 #[no_mangle]
-pub fn insert_lookup(v: i32) -> i32 {
+pub extern "C" fn insert_lookup(v: i32) -> i32 {
     let mut m: BTreeMap<String, i32> = BTreeMap::new();
     m.insert(format!("{}", v), v * 2);
     *m.get(format!("{}", v).as_str()).unwrap()
